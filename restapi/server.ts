d8ecf7d509d97ba5d59a8733900ff35e9f6224d5
Jesus Alonso Garcia <jesus.ag00@gmail.com>
2022-03-08 20:53:08 +0100

    Changes on the rest api + some other enhancements

--- restapi/server.ts
+++ restapi/server.ts
@@ -12,7 +12,7 @@ function connect() {
   // const mongoUrl = "mongodb://localhost:27017/exampleDatabase";
   //This is the url of the connection to the database, currently the database is stored in MongoDB Atlas (A browser version for MongoDB)
   //The user and the password are for this instance only and will be changed for when this is merged.
-  var mongoUrl = process.env.ATLAS;
+  var mongoUrl = process.env.DEDE;
   //Creation of the connection for the database. Pretty simple like any other db.
   mongoose
     .connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
