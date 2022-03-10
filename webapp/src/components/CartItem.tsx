f8a1c12c06f30d4f53b070eb3d3cfd52c62b449e
uo265488 <78318693+uo265488@users.noreply.github.com>
2022-03-10 17:25:43 +0100

    Create CartItem.tsx
    For displaying cart items

---
+++ webapp/src/components/CartItem.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled/types/base";
+import Button from "@mui/material/Button";
+import { CartItemType } from "./CartItemType";
+
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: string) => void;
+};
+
+function CartItem(props :  Props) : JSX.Element {
+  return (
+    <>
+      <div>
+        <h3>{props.item.product.name}</h3>
+        <div className="information">
+          <p>Price: ${props.item.product.price}</p>
+          <p>Total: ${(props.item.units * props.item.product.price).toFixed(2)}</p>
+        </div>
+        <div className="buttons">
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => props.removeFromCart(props.item.product.id)}
+          >
+            -
+          </Button>
+          <p>{props.item.units}</p>
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => props.addToCart(props.item)}
+          >
+            +
+          </Button>
+        </div>
+      </div>
+      <img src={props.item.product.href} alt={props.item.product.name} />
+    </>
+      
+  );
+};
+
+export default CartItem;
