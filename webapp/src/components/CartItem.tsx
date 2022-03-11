1b68430c9d253e6b49a97d69d91ab90607a6c79e
uo265488 <78318693+uo265488@users.noreply.github.com>
2022-03-11 18:08:56 +0100

    Update CartItem.tsx

--- webapp/src/components/CartItem.tsx
+++ webapp/src/components/CartItem.tsx
@@ -1,22 +1,26 @@
 import styled from "@emotion/styled/types/base";
 import Button from "@mui/material/Button";
 import { IProduct } from "../../../restapi/model/Products";
+import { Wrapper } from "./CartItem.styles";
 import { ICartItem } from "./ICartItem";
 
 type Props = {
   item: ICartItem;
-  addToCart: (clickedItem: IProduct) => void;
+  addToCart: (clickedItem: ICartItem) => void;
   removeFromCart: (id: string) => void;
 };
 
-function CartItem(props :  Props) : JSX.Element {
+function CartItem(props: Props): JSX.Element {
+
+  let imageRef: string = require("../static/images/" + props.item.product._id + ".png");
+
   return (
-    <>
+    <Wrapper>
       <div>
         <h3>{props.item.product.name}</h3>
         <div className="information">
-          <p>Price: ${props.item.product.price}</p>
-          <p>Total: ${(props.item.units * props.item.product.price).toFixed(2)}</p>
+          <p>Price: {props.item.product.price} € </p>
+          <p>Total: {(props.item.units * props.item.product.price).toFixed(2)} € </p>
         </div>
         <div className="buttons">
           <Button
@@ -32,15 +36,16 @@ function CartItem(props :  Props) : JSX.Element {
             size="small"
             disableElevation
             variant="contained"
-            onClick={() => props.addToCart(props.item.product)}
+            onClick={() => props.addToCart(props.item)}
           >
             +
           </Button>
         </div>
       </div>
-      <img src={props.item.product.photo} alt={props.item.product.name} />
-    </>
       
+      <img src={imageRef} alt={props.item.product.name} />
+    
+      </Wrapper>
   );
 };
 
