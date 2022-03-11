ea3d66cf575a93ee6849a43cd8f272cda772de51
uo265488 <78318693+uo265488@users.noreply.github.com>
2022-03-11 16:00:10 +0100

    Created Catalogue.tsx component for displaying products

---
+++ webapp/src/routes/Catalogue.tsx
@@ -0,0 +1,36 @@
+import { FormEvent } from "react";
+import { IProduct } from "../../../restapi/model/Products";
+import ProductComponent from "../components/ProductComponent";
+
+interface CatalogueProps {
+    products: IProduct[];
+    addToCart: (clickedItem: IProduct) => void;
+    searchForProducts: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const CatalogueComponent = (props: CatalogueProps) => {
+
+  return (
+    <div className="App">
+      <h1>Product Search App</h1>
+      <form className="searchForm" onSubmit={event => props.searchForProducts(event)}>
+        <input id="searchText" type="text" />
+        <button>Search</button>
+      </form>
+      <div className="products-container">
+
+        {props.products.map((product, i) => {
+          return (
+            <ProductComponent key={i} product={product} onAddToCart={props.addToCart}></ProductComponent>
+
+          );
+        })}
+
+      </div>
+
+    </div>
+  )
+    
+}
+
+export default CatalogueComponent;
