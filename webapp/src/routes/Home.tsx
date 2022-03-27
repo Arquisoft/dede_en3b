a2a0530439a031dbf46d3b08cb3ee9fea7cd3307
Celia <uo276077@uniovi.es>
2022-03-27 21:52:55 +0200

    Routes updated

---
+++ webapp/src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+
+
+
+    return (
+        <React.Fragment>
+            <h2>Welcome</h2>
+        </React.Fragment>
+    );
+};
+
+export default Home;
