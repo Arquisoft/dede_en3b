dc4980ea362af5b38601d727960cba8cae21692d
nic9las <supernik2000@gmail.com>
2022-03-30 20:35:22 +0200

    The orders are shown

--- webapp/src/components/IOrderItem.tsx
+++
@@ -1,6 +0,0 @@
-import { IOrder } from "../../../restapi/model/Order";
-
-
-export interface IOrderItem {
-    product: IOrder,
-}
--- webapp/src/routes/UserOrders.tsx
+++ webapp/src/routes/UserOrders.tsx
@@ -3,6 +3,8 @@ import Grid from "@mui/material/Grid";
 import { StyledButton } from './Product.styles';
 import { IOrder } from "../../../restapi/model/Order";
 import { findOrdersByUser } from "../api/api";
+import OrderItem from "../components/OrderItem";
+import { ICartItem } from "../components/ICartItem";
 
 type Props = {
     orders:IOrder[];
@@ -17,7 +19,10 @@ const UserOrders = ({orders, getUserOrders}:Props) => {
           <h2>Your Orders</h2>
           {orders.length === 0 ? <p>No orders made.</p> : <p></p>}
           {orders.map((order:IOrder) => (
-              order.totalPrice
+               <OrderItem
+               key={order._id.toString()}
+               item={order}
+               />
           ))}
         </Wrapper>
       );
