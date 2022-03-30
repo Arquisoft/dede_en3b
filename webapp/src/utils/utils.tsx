7a26f5118ef62fc4f7b58947fd850ecef3973660
uo265488 <uo265488@uniovi.es>
2022-03-30 19:15:44 +0200

    Create utils.tsx

---
+++ webapp/src/utils/utils.tsx
@@ -0,0 +1,10 @@
+import { ICartItem } from '../components/ICartItem';
+ 
+export function computeTotalPrice(cart: ICartItem[]) {
+
+    var totalPrice = 0;
+    cart.forEach(
+        i => totalPrice += i.product.price * i.units
+    )
+    return totalPrice;
+}
