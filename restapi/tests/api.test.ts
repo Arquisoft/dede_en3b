6e6c3aedef2956acc11bac15735c85b103edbf5f
gsusag00 <jesus.ag00@gmail.com>
2022-03-30 17:56:32 +0200

    All the test pass so far, still not really that rigorous

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -1,32 +1,37 @@
 import request, {Response} from 'supertest';
 import express, { Application } from 'express';
-import * as http from 'http';
+const server = require("./test.server");
 import bp from 'body-parser';
 import cors from 'cors';
 import api from '../api';
+import { IProduct } from '../model/Products';
+import {Types} from 'mongoose';
 
 let app:Application;
-let server:http.Server;
 
 beforeAll(async () => {
-    app = express();
-    const port: number = 5000;
-    const options: cors.CorsOptions = {
-        origin: ['http://localhost:3000']
-    };
-    app.use(cors(options));
-    app.use(bp.json());
-    app.use("/api", api)
+    // app = express();
+    // const port: number = 5000;
+    // const options: cors.CorsOptions = {
+    //     origin: ['http://localhost:3000']
+    // };
+    // app.use(cors(options));
+    // app.use(bp.json());
+    // app.use("/api", api)
 
-    server = app.listen(port, ():void => {
-        console.log('Restapi server for testing listening on '+ port);
-    }).on("error",(error:Error)=>{
-        console.error('Error occured: ' + error.message);
-    });
+    // server = app.listen(port, ():void => {
+    //     console.log('Restapi server for testing listening on '+ port);
+    // }).on("error",(error:Error)=>{
+    //     console.error('Error occured: ' + error.message);
+    // });
+    await server.startDB();
+    app = await server.startServer();
+    server.addProducts();
 });
 
 afterAll(async () => {
-    server.close() //close the server
+    await server.closeServer(); //close the server
+    await server.closeDB();
 })
 
 describe('user ', () => {
@@ -62,8 +67,19 @@ describe('product', () => {
      * Test that we can get a product based on its id.
      */
     it('A product to be listed', async() => {
-        let id:string = "62222f0768d0bc49564b740d";
-        const response:Response = await request(app).get('/api/products/' + id);
+        let toBeFound:IProduct = {
+            "_id": new Types.ObjectId("6227ae61e18344de6a6f9278"),
+            "name":"Pantalón de chandal",
+            "description":"Pantalones de chandal para hacer deporte",
+            "photo":"pantalon_chandal.png",
+            "type":"Pantalon",
+            "price":20.55,
+            "__v":0
+        } as IProduct;
+        //The response is good.
+        const response:Response = await request(app).get('/api/products/' + toBeFound._id);
         expect(response.statusCode).toBe(200);
+        console.log(response.body);
+        //The object is the one that we were looking for.
     });
 });
