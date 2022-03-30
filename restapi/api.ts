50178c403cfb9e94d36666d9bcfc26e9600b0342
gsusag00 <jesus.ag00@gmail.com>
2022-03-30 18:08:25 +0200

    Some changes in the RESTApi, to ensure that all tests pass and a new dependency added

--- restapi/api.ts
+++ restapi/api.ts
@@ -10,21 +10,14 @@ const Order = require('./model/Order');
 var mongoose = require('mongoose');
 const api:Router = express.Router();
 
-// app.use(function(req,res,next){
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+interface User {
+    name: string;
+    email: string;
+}
 
-
-// interface User {
-//     name: string;
-//     email: string;
-// }
-
-// //This is not a restapi as it mantains state but it is here for
-// //simplicity. A database should be used instead.
-// let users: Array<User> = [];
+//This is not a restapi as it mantains state but it is here for
+//simplicity. A database should be used instead.
+let users: Array<User> = [];
 
 
 //The code here answers to the get petition over "users/list", basically shows all the users that are currently stored in the database.
@@ -76,7 +69,7 @@ api.get("/products/:id",async (req: Request, res:Response): Promise<Response> =>
     console.log(objID);
     const products:IProduct = await Products.findOne({_id: objID});
     if(!products) {
-      return res.status(404).json({message: 'Product with name "${name}" not found'});
+      return res.status(404).json({message: 'Product with name "${objID}" not found'});
     }
     return res.status(200).send(products);
 });
