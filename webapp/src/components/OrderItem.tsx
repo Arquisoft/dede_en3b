27ca07842c295e1f4bb0d4fe92727af420945266
nic9las <supernik2000@gmail.com>
2022-04-03 13:19:03 +0200

    Reformatted date

--- webapp/src/components/OrderItem.tsx
+++ webapp/src/components/OrderItem.tsx
@@ -10,9 +10,9 @@ function OrderItem(props: Props): JSX.Element {
   return (
     <Wrapper className="order-item">
       <div>
-        <h3>{props.item.date}</h3>
+        <h3>{new Date(props.item.date).toUTCString()}</h3>
         <div className="information">
-          <p>Adress: [ Country: {props.item.address.country}, Region: {props.item.address.region}, Postal Code: {props.item.address.postal_code}, Street: {props.item.address.street} ]</p>
+          <p>Country: {props.item.address.country}, Region: {props.item.address.region}, Postal Code: {props.item.address.postal_code}, Street: {props.item.address.street}</p>
           <p>Total: {(props.item.totalPrice).toFixed(2)} € </p>
         </div>
         </div>
