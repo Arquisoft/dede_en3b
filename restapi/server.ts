e120952ac7669191310f47b011f390deaf19c1d2
mocoma <mocomaminecraft@gmail.com>
2022-04-03 18:35:55 +0200

    Added new API endpoint for solid

--- restapi/server.ts
+++ restapi/server.ts
@@ -8,47 +8,56 @@ require('dotenv').config();
 
 //mongodb+srv://username:password@pruebaasw.dxqcq.mongodb.net/exampleDatabase?retryWrites=true&w=majority
 
-function connect() {
-  // const mongoUrl = "mongodb://localhost:27017/exampleDatabase";
-  //This is the url of the connection to the database, currently the database is stored in MongoDB Atlas (A browser version for MongoDB)
-  //The user and the password are for this instance only and will be changed for when this is merged.
-  var mongoUrl = process.env.DEDE;
-  //Creation of the connection for the database. Pretty simple like any other db.
-  mongoose
-    .connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-      //All the code that was previously in the server.ts
-      const app: Application = express();
+function async connect() {
+	const app: Application = express();
+	const port: number = 5000;
 
-      const port: number = 5000;
+	console.log("Application started: " + options.origin);
 
-      const options: cors.CorsOptions = {
-        origin: ["http://localhost:3000"],
-      };
+	const options: cors.CorsOptions = {
+		origin: ["http://localhost:3000"],
+	};
 
-      console.log("Application started: " + options.origin);
+	const metricsMiddleware: RequestHandler = promBundle({
+		includeMethod: true,
+	});
+	app.use(metricsMiddleware);
 
-      const metricsMiddleware: RequestHandler = promBundle({
-        includeMethod: true,
-      });
-      app.use(metricsMiddleware);
+	app.use(cors(options));
+	app.use(bp.json());
 
-      app.use(cors(options));
-      app.use(bp.json());
+	await restapi();
+	await solid();
 
-      app.use("/api", api);
-      app
-        .listen(port, (): void => {
-          console.log("Restapi listening on " + port);
-        })
-        .on("error", (error: Error) => {
-          console.error("Error occured: " + error.message);
-        });
-    });
+	app
+		.listen(port, (): void => {
+			console.log("Restapi listening on " + port);
+		})
+		.on("error", (error: Error) => {
+			console.error("Error occured: " + error.message);
+		});
+}
 
-    const db = mongoose.connection;
-    db.on("error",console.error.bind(db,"Error on connection: "));
+function restapi(app: Application) {
+	// const mongoUrl = "mongodb://localhost:27017/exampleDatabase";
+	//This is the url of the connection to the database, currently the database is stored in MongoDB Atlas (A browser version for MongoDB)
+	//The user and the password are for this instance only and will be changed for when this is merged.
+	var mongoUrl = process.env.DEDE;
+	//Creation of the connection for the database. Pretty simple like any other db.
+	mongoose
+		.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+		.then(() => {
+
+			app.use("/api", api);
+		});
+
+	const db = mongoose.connection;
+	db.on("error",console.error.bind(db,"Error on connection: "));
 };
+
+function solid(app: Application) {
+	app.use("/solid", solid);
+}
 
 //Timeout to allow the database to finish building.
 setTimeout(connect,1000);
---
+++ restapi/solid.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response, Router } from "express";
+import { SolidConnection } from "./SOLID/API";
+import { VCARD, FOAF } from "@inrupt/vocab-common-rdf";
+
+const api: Router = express.Router();
+
+let connection: SolidConnection = 
+	new SolidConnection("https://www.solidcommunity.net");
+
+api.get("/login", async (req: Request, res: Response) => {
+	if(req.query.provider !== null)
+		connection = new SolidConnection(req.query.provider);
+
+	if(!connection.isLoggedIn())
+		connection.login("http://localhost:5000/api/redirect", res);
+});
+
+api.get("/redirect", async (req: Request, res: Response) => {
+	await connection
+		.tryHandleRedirect(`http://localhost:5000/api${req.url}`);
+
+	res.redirect("/");
+});
+
+api.get("/address", async (req: Request, res: Response): Promise<Response> => {
+	if(!connection.isLoggedIn()) 
+		return res.status(403).json(
+			{ message: "User not logged in" }
+		);
+
+	let url = await connection.fetchDatasetFromUser("profile/card")
+		.getThingAsync(connection.getWebId().href)
+		.then(thing => thing.getUrl(VCARD.hasAddress));
+
+	let address = await connection.fetchDatasetFromUser("profile/card")
+		.getThingAsync(url ?? "")
+		.then(thing => ({
+			country_name: thing.getString(VCARD.country_name),
+			locality: thing.getString(VCARD.locality),
+			postal_code: thing.getString(VCARD.postal_code),
+			region: thing.getString(VCARD.region),
+			street_address: thing.getString(VCARD.street_address),
+		}));
+
+	return res.status(200).json(address);
+});
+
+api.get("/webId", async (req: Request, res: Response): Promise<Response> => {
+	if(!connection.isLoggedIn()) 
+		return res.status(403).json(
+			{ message: "User not logged in" }
+		);
+
+	return res.status(200).json({ webId: connection.getWebId() });
+});
+
+api.get("/name", async (req: Request, res: Response): Promise<Response> => {
+	if(!connection.isLoggedIn()) 
+		return res.status(403).json(
+			{ message: "User not logged in" }
+		);
+
+	let name = await connection.fetchDatasetFromUser("profile/card")
+		.getThingAsync(connection.getWebId().href)
+		.then(thing => thing.getString(FOAF.name));
+	return res.status(200).json({ name: name });
+});
+
+export default api;
