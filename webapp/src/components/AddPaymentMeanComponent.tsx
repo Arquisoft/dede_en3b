8b8e4ddf2e19402092789e407985c2ff2cb88f2d
uo265488 <uo265488@uniovi.es>
2022-04-06 17:40:30 +0200

    Update AddPaymentMeanComponent.tsx

--- webapp/src/components/AddPaymentMeanComponent.tsx
+++ webapp/src/components/AddPaymentMeanComponent.tsx
@@ -1,7 +1,6 @@
-import { Button, Container, FormLabel, Grid, Input, InputLabel, InputLabelProps, Radio } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import { Address } from "../../../restapi/model/Order";
+import { Box, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { getSolidAddress } from "../api/api";
 import { getShippingCosts } from "../api/ShippingApi";
 
@@ -11,7 +10,6 @@ interface AddPaymentMeanComponentProps {
     makeOrder: () => void
 }
 
-
 export function AddPaymentMeanComponent(props: AddPaymentMeanComponentProps): JSX.Element {
 
     // The selected paymentmean
@@ -20,7 +18,7 @@ export function AddPaymentMeanComponent(props: AddPaymentMeanComponentProps): JS
     const [discountCode, setDiscountCode] = useState('');
     //Shipping costs
     const [shippingCosts, setShippingCosts] = useState(0);
- 
+
     const computeShippingCosts = async () => {
         const res = await getShippingCosts(await getSolidAddress());
 
@@ -32,7 +30,7 @@ export function AddPaymentMeanComponent(props: AddPaymentMeanComponentProps): JS
     React.useEffect(() => {
         computeShippingCosts();
         console.log(shippingCosts);
-      }, []);
+    }, []);
 
     // This function will be triggered when a radio button is selected
     const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,62 +43,58 @@ export function AddPaymentMeanComponent(props: AddPaymentMeanComponentProps): JS
         props.makeOrder();
     };
 
-    const handleChangeDiscount = () =>
-    {
+    const handleChangeDiscount = () => {
 
     }
 
-   // if (props.connection.isLoggedIn()) {
-   //     return <Navigate to='/login'></Navigate>
+    // if (props.connection.isLoggedIn()) {
+    //     return <Navigate to='/login'></Navigate>
     //} else {
 
 
-        return <div className="container">
-            <div>
-                <h2>Choose a payment mean: </h2>
-                <fieldset>
-                    <legend>Please select the payment mean you want to use:</legend>
-                    <p>
-                        <input
-                            type="radio"
-                            name="paymentmean"
-                            value="Paypal"
-                            id="paypal"
-                            onChange={radioHandler}
-                            checked
-                        />
-                        <label htmlFor="paypal">Paypal</label>
-                    </p>
+    return <div className="container">
+        <div>
+            <h2>Choose a payment mean: </h2>
+            <fieldset>
+                <legend>Please select the payment mean you want to use:</legend>
+                <p>
+                    <input
+                        type="radio"
+                        name="paymentmean"
+                        value="Paypal"
+                        id="paypal"
+                        onChange={radioHandler}
+                        checked
+                    />
+                    <label htmlFor="paypal">Paypal</label>
+                </p>
 
-                    <p>
-                        <input
-                            type="radio"
-                            name="paymentmean"
-                            value="Creditcard"
-                            id="creditcard"
-                            onChange={radioHandler}
-                        />
-                        <label htmlFor="creditcard">CreditCard</label>
-                    </p>
-                </fieldset>
-            </div>
-            <div className="container">
-                <h2> Discount code: </h2>
-                <input type="text" name="discount" onChange={handleChangeDiscount} placeholder="Introduce your discount code." id="discount"></input>
-            </div>
-            
-            <div>
-                
-                <h2>Shipping {shippingCosts} € </h2>
-                <h2>Cart Items {props.totalCost} € </h2>
-                <h1>TOTAL (IVA included) {props.totalCost + shippingCosts}€ </h1>
-            </div>
-            
-        
-            <Link to='/'><Button onClick={handleConfirmOrder}>CONFIRM ORDER</Button></Link>
-
-      
+                <p>
+                    <input
+                        type="radio"
+                        name="paymentmean"
+                        value="Creditcard"
+                        id="creditcard"
+                        onChange={radioHandler}
+                    />
+                    <label htmlFor="creditcard">CreditCard</label>
+                </p>
+            </fieldset>
         </div>
-    
+        <div className="container">
+            <h2> Discount code: </h2>
+            <input type="text" name="discount" onChange={handleChangeDiscount} placeholder="Introduce your discount code." id="discount"></input>
+        </div>
+        <div>
+
+            <h2>Shipping {shippingCosts} € </h2>
+            <h2>Cart Items {props.totalCost} € </h2>
+            <h1>TOTAL (IVA included) {(props.totalCost + shippingCosts).toFixed(2)}€ </h1>
+        </div>
+
+        <Link to='/shipping/confirmation'><Button onClick={handleConfirmOrder}>CONFIRM ORDER</Button></Link>
+
+    </div>
+
 
 }
