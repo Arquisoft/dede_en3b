ba7023dd2a907ca2251b0b1558519bb97b3a6a95
uo265488 <uo265488@uniovi.es>
2022-04-25 21:11:03 +0200

    Update OrderItem.tsx

--- webapp/src/components/OrderItem.tsx
+++ webapp/src/components/OrderItem.tsx
@@ -12,7 +12,7 @@ function OrderItem(props: Props): JSX.Element {
       <div>
         <h3>{props.item.date}</h3>
         <div className="information">
-          <p>Adress: [ Country: {props.item.address.country}, Region: {props.item.address.region}, Postal Code: {props.item.address.postal_code}, Street: {props.item.address.street} ]</p>
+          <p>Adress: [ Country: {props.item.address.country_name}, Region: {props.item.address.region}, Postal Code: {props.item.address.postal_code}, Street: {props.item.address.street_address} ]</p>
           <p>Total: {(props.item.totalPrice).toFixed(2)} € </p>
         </div>
         </div>
