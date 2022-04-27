20d3c7c9e7e14fe0b7d6aab06a328350d05e00c7
uo265488 <uo265488@uniovi.es>
2022-04-27 19:04:35 +0200

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line
-import React, { useState, useEffect, FormEvent} from 'react';
-import  {findOrdersByUser, addOrder, getSolidWebId, getSolidAddress} from './api/api';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { findOrdersByUser, addOrder, getSolidWebId, getSolidAddress } from './api/api';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
-import {IOrder, Address} from './shared/shareddtypes';
+import { IOrder, Address } from './shared/shareddtypes';
 import Cart from './routes/Cart';
 import Catalogue from './routes/Catalogue';
 import IndividualProduct from './routes/IndividualProduct';
@@ -23,11 +23,11 @@ import AddressForm from './components/checkout/AddressForm';
 import Checkout from './components/checkout/Checkout';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './redux/store';
-import {emptyCart} from "./redux/slices/cartSlice"
+import { emptyCart } from "./redux/slices/cartSlice"
 
 function App(): JSX.Element {
 
-// eslint-disable-next-line
+  // eslint-disable-next-line
   const [value, setValue] = useState('');
   //Address
   // eslint-disable-next-line
@@ -39,19 +39,19 @@ function App(): JSX.Element {
   //Shipping
   // eslint-disable-next-line
   const [shippingCosts, setShippingCosts] = useState(0);
-  
-  
+
+
 
   //Cart
   const cart = useSelector((state: RootState) => state.cart.value);
   const dispatch = useDispatch();
 
-  
 
 
-    
 
-  
+
+
+
   /**
   * Function to restore the default values of the cart.
   */
@@ -74,11 +74,11 @@ function App(): JSX.Element {
 
   }
 
-     //Orders
+  //Orders
   const [orders, setOrders] = useState<IOrder[]>([]);
 
-  const getUserOrders = async (orders:IOrder[], WebId:string) =>{
-    var ordersFound : IOrder[];
+  const getUserOrders = async (orders: IOrder[], WebId: string) => {
+    var ordersFound: IOrder[];
     ordersFound = await findOrdersByUser(WebId);
     setOrders(ordersFound);
   }
@@ -88,25 +88,25 @@ function App(): JSX.Element {
 
     <BrowserRouter>
 
-      <NavigationBar/>
+      <NavigationBar />
 
       <Routes>
-        <Route path="/" element={ <Home />} ></Route>
+        <Route path="/" element={<Home />} ></Route>
         <Route path="login" element={<Login></Login>}> </Route>
-        <Route path="cart" element={<Cart cart={cart}/>} />
+        <Route path="cart" element={<Cart cart={cart} />} />
         <Route path="/" element={<Catalogue />} />
-        <Route path="shop" element={<Catalogue /> } />
-        <Route path="products/:id" 
+        <Route path="shop" element={<Catalogue />} />
+        <Route path="products/:id"
           element={
             <IndividualProduct product={null as any} />
           }
         />
-		
-        <Route path="shipping/payment" element={<Checkout makeOrder={makeOrder} cart={cart}></Checkout>}/>
-        <Route path="shipping/payment" element={<AddPaymentMeanComponent  setPaymentMean={setPaymentMean}
-          totalCost={computeTotalPrice(cart)} makeOrder={makeOrder} ></AddPaymentMeanComponent>} ></Route>      
+
+        <Route path="shipping/payment" element={<Checkout makeOrder={makeOrder}></Checkout>} />
+        <Route path="shipping/payment" element={<AddPaymentMeanComponent setPaymentMean={setPaymentMean}
+          totalCost={computeTotalPrice(cart)} makeOrder={makeOrder} ></AddPaymentMeanComponent>} ></Route>
         <Route path="shipping/confirmation" element={<ConfirmationComponent ></ConfirmationComponent>}></Route>
-        <Route path="orders/find" element={<UserOrders orders={orders} getUserOrders={getUserOrders}/> } />
+        <Route path="orders/find" element={<UserOrders orders={orders} getUserOrders={getUserOrders} />} />
       </Routes>
 
     </BrowserRouter>
