7ee04850e182f3bc23738fc26bb5162a5deef67f
uo265488 <uo265488@uniovi.es>
2022-04-30 22:05:34 +0200

    Update Review.tsx

--- webapp/src/components/checkout/Review.tsx
+++ webapp/src/components/checkout/Review.tsx
@@ -6,23 +6,27 @@ import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
 import { computeTotalPrice } from '../../utils/utils';
 import { getShippingCosts } from '../../api/ShippingApi';
-import { getSolidAddress } from '../../api/api';
 import { useState } from 'react';
 // eslint-disable-next-line
-import { AnalyticsOutlined } from '@mui/icons-material';
 import { PaymentData } from './Checkout';
-import { Address, ICartItem } from '../../shared/shareddtypes';
+import { Address} from '../../shared/shareddtypes';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
+import { Alert } from '@mui/material';
 
 interface ReviewProps {
     paymentData: PaymentData,
-    address: Address
+    address: Address,
+    setShippingPossible: (b: boolean) => void 
 }
+
+const ERROR_CODE = -1;
 
 export default function Review(props: ReviewProps): JSX.Element {
 
     const cart = useSelector((state: RootState) => state.cart.value);
+
+    const [shippingCostsAlert, setShippingCostsAlert] = useState(false);
 
     const defaultAddress = {
         country_name: "string",
@@ -35,24 +39,17 @@ export default function Review(props: ReviewProps): JSX.Element {
 
     //Shipping costs
     const [shippingCosts, setShippingCosts] = useState(0);
-    // //Solid Address
-    // const [solidAddress, setSolidAddress] = useState(defaultAddress);
-
-    // const computeShippingCosts = async () => {
-    //     const address = await getSolidAddress();
-    //     // setSolidAddress(address);
-
-    //     // const res = await getShippingCosts(address);
-    //     console.log(res);
-
-    //     setShippingCosts(Number((res * 0.10).toFixed(2)));
-    // };
 
     const computeShippingCosts = async () => {
         const res = await getShippingCosts(props.address);
-        console.log(res);
 
-        setShippingCosts(Number((res * 0.10).toFixed(2)));
+        if (res === ERROR_CODE) {
+            setShippingCostsAlert(true);
+        } else {
+            setShippingCosts(Number((res * 0.10).toFixed(2)));
+            props.setShippingPossible(true);
+        }
+            
     };
 
     React.useEffect(() => {
@@ -67,6 +64,7 @@ export default function Review(props: ReviewProps): JSX.Element {
             <Typography variant="h6" gutterBottom>
                 Order summary
             </Typography>
+            {shippingCostsAlert && <Alert severity="error">Sorry, the selected address could not be found by our shipping company. Select a valid address or add a new one to the POD. </Alert>}
             <List disablePadding>
                 {cart.map((product) => (
                     <ListItem key={product.product.name} sx={{ py: 1, px: 0 }}>
@@ -83,7 +81,7 @@ export default function Review(props: ReviewProps): JSX.Element {
                 <ListItem sx={{ py: 1, px: 0 }}>
                     <ListItemText primary="Shipping Costs" />
                     <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-                        {shippingCosts === 0 ? "Loading..." : shippingCosts + "€"}
+                        {shippingCosts === 0 ? (shippingCostsAlert ? "Address not found." : "Loading...") : shippingCosts + "€"}
                     </Typography>
                 </ListItem>
                 <ListItem sx={{ py: 1, px: 0 }}>
@@ -95,22 +93,6 @@ export default function Review(props: ReviewProps): JSX.Element {
             </List>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
-                    {/* <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-                        Shipping
-                    </Typography>
-                    <Typography gutterBottom>Mario Alfombras</Typography>
-                    <Typography gutterBottom>
-                        {solidAddress.street_address === defaultAddress.street_address ? "Loading..." : solidAddress.street_address}
-                    </Typography>
-                    <Typography gutterBottom>
-                        {solidAddress.street_address === defaultAddress.street_address ? "Loading..." : solidAddress.locality + " " + solidAddress.postal_code}
-                    </Typography>
-                    <Typography gutterBottom>
-                        {solidAddress.street_address === defaultAddress.street_address ? "Loading..." : solidAddress.region}
-                    </Typography>
-                    <Typography gutterBottom>
-                        {solidAddress.street_address === defaultAddress.street_address ? "Loading..." : solidAddress.country_name}
-                    </Typography> */}
                     <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
                         Shipping
                     </Typography>
