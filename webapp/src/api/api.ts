b8296d736b417f5baafa7b9de059b5a5f5c49fe5
mocoma <mocomaminecraft@gmail.com>
2022-05-03 13:20:41 +0200

    changed endpoints

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,7 +1,7 @@
 import {IProduct, IOrder, Address, Review, ICartItem} from '../shared/shareddtypes';
 
-//const apiEndPoint = process.env.REACT_APP_API_URI || 'https://dedeen3b-restapi.herokuapp.com/api'
-const apiEndPoint = process.env.REACT_APP_API_URI || 'http://localhost:5000/api';
+const apiEndPoint = process.env.REACT_APP_API_URI || 'https://dedeen3b-restapi.herokuapp.com/api'
+//const apiEndPoint = process.env.REACT_APP_API_URI || 'http://localhost:5000/api';
 const solidEndPoint = apiEndPoint.replace('/api', '/solid');
 
 /**
