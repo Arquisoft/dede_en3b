8718644cfe45437ef36ee810fa8d8f14071fc18c
mocoma <mocomaminecraft@gmail.com>
2022-05-03 20:32:06 +0200

    Back to the origins

--- restapi/routers/solid.ts
+++ restapi/routers/solid.ts
@@ -71,7 +71,7 @@ solid.get("/redirect", async (req: Request, res: Response) => {
 
 	SessionStorage.instance.set(connection);
 	req.session.webId = connection.getWebId();
-	//req.session.save();
+	req.session.save();
 
 	console.log("Logged in " + req.session.webId)
 
