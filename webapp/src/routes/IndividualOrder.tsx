7f2a18599a5d29e591cbbbadfd44ee9c1508cf91
Celia <uo276077@uniovi.es>
2022-05-04 18:08:22 +0200

    Individual order improved

--- webapp/src/routes/IndividualOrder.tsx
+++ webapp/src/routes/IndividualOrder.tsx
@@ -7,11 +7,37 @@ import IndividualOrderProduct from "../components/IndividualOrderProduct";
 import { getSolidWebId } from "../api/api";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
 
 type IndividualOrderProps = {
     order: IOrder;
 }
+
+function BreadcrumbsIndividualOrder(props:any) {
+    return(
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" href="/" >
+          <Typography
+          variant='h6'
+          sx={{color: 'text.secondary'}}>
+              Home
+          </Typography>
+        </Link>
+        <Link underline="hover" href="order" >
+        <Typography variant='h6'
+          sx={{color: 'text.secondary'}}>
+              Orders
+          </Typography>
+        </Link>
+        <Typography variant='h6'
+        sx={{color: 'text.secondary'}}>
+            {props.order}
+        </Typography>
+      </Breadcrumbs>
+    );
+  }
 
 const IndividualProduct = (props: IndividualOrderProps) => {
     const { id } = useParams();
@@ -42,15 +68,17 @@ const IndividualProduct = (props: IndividualOrderProps) => {
 
     if (typeof order === "undefined") {
         return (
-            <React.Fragment>
+            <Box sx={{ bgcolor: 'background.default', height: '100vh', pl: 2, pt: 2}}>
+                <BreadcrumbsIndividualOrder order='Not exists'/>
                 <h2>The order does not exist</h2>
-            </React.Fragment>
+            </Box>
         );
     } else {
         if (order.webId === webId) {
 
             return (
-                <Box sx={{ bgcolor: 'background.default', padding: 2, height: '90vh', display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ bgcolor: 'background.default', padding: 2, height: '100vh', display: 'flex', flexDirection: 'column' }}>
+                    <BreadcrumbsIndividualOrder order='Not exists'/>
                     <Typography
                         variant='h3'
                         sx={{ color: 'text.default', pt: 3, pb: 2 }}
@@ -63,7 +91,7 @@ const IndividualProduct = (props: IndividualOrderProps) => {
                         sx={{ pt: 0, display: 'flex', flexWrap: 'wrap', flexDirection: 'column', width: '80%', height:'50%' }}>
                         {order.orderProducts.map(product => {
                             return (
-                                <Grid item xs={8} sm={3}>
+                                <Grid item xs={8} sm={6} md={3}>
                                     <IndividualOrderProduct
                                         key={product.id}
                                         id={product.id}
@@ -90,9 +118,10 @@ const IndividualProduct = (props: IndividualOrderProps) => {
             );
         } else {
             return (
-                <React.Fragment>
+                <Box sx={{ bgcolor: 'background.default', height: '90vh', pt: 2, pl: 2}}>
+                    <BreadcrumbsIndividualOrder order='No permission'/>
                     <h2>You don't have permission to view this order!</h2>
-                </React.Fragment>);
+                </Box>);
         }
     }
 }
